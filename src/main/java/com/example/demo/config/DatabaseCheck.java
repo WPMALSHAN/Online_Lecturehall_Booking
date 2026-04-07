b7915a0e7e2a1d5fa2496129eb5a26513d300f14
Pasindu malshan <pasindumalshan934@gmail.com>
2026-04-07 15:27:12 +0530

    Create The Project

---
+++ src/main/java/com/example/demo/config/DatabaseCheck.java
@@ -0,0 +1,23 @@
+package com.example.demo.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseCheck {
+
+    @Bean
+    public CommandLineRunner checkConnection(DataSource dataSource) {
+        return args -> {
+            try {
+                dataSource.getConnection().close();
+                System.out.println("🔥 DATABASE CONNECTED SUCCESSFULLY 🔥");
+            } catch (Exception e) {
+                System.out.println("❌ DATABASE CONNECTION FAILED ❌");
+            }
+        };
+    }
+}
